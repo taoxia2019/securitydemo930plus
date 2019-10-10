fe7d8b7aadcc0606a2d40eebf9b40107d0928a83
taoxia <781775099@qq.com>
2019-10-10 18:46:15 +0800

    解决静态文件加载问题

--- src/main/java/com/lena/security/SpringSecurytyConfig.java
+++ src/main/java/com/lena/security/SpringSecurytyConfig.java
@@ -4,7 +4,10 @@ import com.lena.security.authentication.MyAuthenticationSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -21,13 +24,22 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * @Date 2019/10/9 16:13
  * @Version 1.0
  */
+@Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) //开启security注解
 public class SpringSecurytyConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsService userDetailsService;
     @Autowired
     private MyAuthenticationSuccessHandler myAuthenticationSuccessHandler;
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -43,8 +55,6 @@ public class SpringSecurytyConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-
-
         http.authorizeRequests()
                 .antMatchers("/static/**",
                         "/login",
@@ -68,6 +78,10 @@ public class SpringSecurytyConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         //解决静态资源被拦截的问题
-        web.ignoring().antMatchers("/css/**");
+        web.ignoring().antMatchers("/xadmin/**");
+        web.ignoring().antMatchers("/treetable-lay/**");
+        web.ignoring().antMatchers("/dtree/**");
     }
+
+
 }
